c79d2b9641f26bf0f9af3ab1b34a6673e7bd6706
exiobao <bob.bao@ericsson.com>
2019-07-22 22:43:09 +0800

    python demo

---
+++ task/demo/strongPWDValidation.py
@@ -0,0 +1,12 @@
+#! python3
+# -*- coding: utf-8 -*-
+
+' strong password validation'
+
+import re
+def strongPasswordValidation(pwd):
+    regex = r'(([A-Z])?([a-z])?(\d)?){8,}'
+    matcher = re.compile(regex)
+    print(matcher.search(pwd).group())
+if __name__ == '__main__':
+    strongPasswordValidation('azA8fmskal')
